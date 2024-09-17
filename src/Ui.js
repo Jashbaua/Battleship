7671ca95c1cf9a0ee3e2e7459273db12e179b074
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-09-17 20:19:20 +0530

    Add pass device waiting screen in two players

--- src/Ui.js
+++ src/Ui.js
@@ -3,17 +3,18 @@ const board1 = document.querySelector("#board1");
 const b1name = document.querySelector("#board1-name");
 const board2 = document.querySelector("#board2");
 const b2name = document.querySelector("#board2-name");
-const instruction=document.querySelector('#instruction')
+const instruction = document.querySelector("#instruction");
+const passDevice=document.querySelector('#pass-device')
 
 export default function renderUi(player1, player2) {
 	b1name.textContent = player1.name;
 	b2name.textContent = player2.name;
 
 	board1.textContent = "";
-	player1.board.getBoard().forEach((array,row) =>
-		array.forEach((data,col) => {
+	player1.board.getBoard().forEach((array, row) =>
+		array.forEach((data, col) => {
 			let cell = document.createElement("div");
-			cell.id=`${row}${col}`
+			cell.id = `${row}${col}`;
 			cell.classList.add("cell");
 			if (typeof data == "object") cell.classList.add("ship");
 			else if (data == 1) cell.classList.add("shot");
@@ -23,10 +24,10 @@ export default function renderUi(player1, player2) {
 	);
 
 	board2.textContent = "";
-	player2.board.getBoard().forEach((array,row) =>
-		array.forEach((data,col) => {
+	player2.board.getBoard().forEach((array, row) =>
+		array.forEach((data, col) => {
 			let cell = document.createElement("div");
-			cell.id=`${row}${col}`
+			cell.id = `${row}${col}`;
 			cell.classList.add("cell");
 			if (typeof data == "object") cell.classList.add("ship");
 			else if (data == 1) cell.classList.add("shot");
@@ -36,6 +37,25 @@ export default function renderUi(player1, player2) {
 	);
 }
 
-export function setInstruction(str) {
-	instruction.textContent=str
+function setInstruction(str) {
+	instruction.textContent = str;
 }
+
+function setActiveBoard(board) {
+	if (board == "board1") {
+		board1.classList.add("active");
+		board2.classList.remove("active");
+	} else {
+		board2.classList.add("active");
+		board1.classList.remove("active");
+	}
+}
+
+function showPassDevice() {
+	passDevice.style.display='block'
+}
+
+function hidePassDevice() {
+	passDevice.style.display='none'
+}
+export { setInstruction, setActiveBoard,showPassDevice,hidePassDevice };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import "./style.css";
 import Player from "./Player";
-import renderUi, { setInstruction } from "./Ui";
+import renderUi, { setInstruction ,setActiveBoard,showPassDevice,hidePassDevice} from "./Ui";
 
 //Dom cache
 const onePlayer = document.querySelector("#one-player");
 const twoPlayer = document.querySelector("#two-player");
 const board1 = document.querySelector("#board1");
 const board2 = document.querySelector("#board2");
+const passDevice=document.querySelector('#pass-device>button')
 
 //Binding events
 onePlayer.addEventListener("click", initOnePlayer);
 twoPlayer.addEventListener("click", initTwoPlayer);
+passDevice.addEventListener("click",toggleActivePlayer)
 
-let player1, player2, activePlayer, inactivePlayer;
+let player1, player2, activePlayer;
 
 initOnePlayer();
 
@@ -29,6 +31,9 @@ function initOnePlayer() {
 	player1 = new Player("You");
 	player2 = new Player("Computer");
 	player2.board.hide();
+	setActiveBoard('board2')
+	board2.removeEventListener("click", board2ClickHandler);
+	board1.removeEventListener("click", board1ClickHandler);
 	board2.addEventListener("click", onePlayerClickHandler);
 	setboards();
 	renderUi(player1, player2);
@@ -37,28 +42,33 @@ function initOnePlayer() {
 function initTwoPlayer() {
 	player1 = new Player("Player1");
 	player2 = new Player("Player2");
+	board2.removeEventListener("click", onePlayerClickHandler);
 	toggleActivePlayer();
 	setboards();
 	renderUi(player1, player2);
 }
 
 function toggleActivePlayer() {
+	hidePassDevice()
 	if (activePlayer == player1) {
 		activePlayer = player2;
 		player1.board.hide();
 		player2.board.unhide();
 		board1.addEventListener("click", board1ClickHandler);
 		board2.removeEventListener("click", board2ClickHandler);
+		setActiveBoard('board1')
 		setInstruction("Player2's turn");
-		return;
 	}
-	activePlayer = player1;
-	player2.board.hide();
-	player1.board.unhide();
-	board2.addEventListener("click", board2ClickHandler);
-	board1.removeEventListener("click", board1ClickHandler);
-	setInstruction("Player1's turn");
-	return;
+	else {
+		activePlayer = player1;
+		player2.board.hide();
+		player1.board.unhide();
+		board2.addEventListener("click", board2ClickHandler);
+		board1.removeEventListener("click", board1ClickHandler);
+		setActiveBoard('board2')
+		setInstruction("Player1's turn");
+	}
+	renderUi(player1,player2)
 }
 function board2ClickHandler(e) {
 	let y = e.target.id[0],
@@ -66,9 +76,9 @@ function board2ClickHandler(e) {
 	if (player2.board.receiveAttack(x, y)) {
 		if (player2.board.areAllSunk()) {
 			setInstruction("Player1 won!");
-			board2.removeEventListener("click", board2ClickHandler);
-		} else toggleActivePlayer();
-		renderUi(player1, player2);
+		} else showPassDevice();
+		board2.removeEventListener("click", board2ClickHandler);
+		renderUi(player1,player2)
 	}
 }
 function board1ClickHandler(e) {
@@ -77,9 +87,9 @@ function board1ClickHandler(e) {
 	if (player1.board.receiveAttack(x, y)) {
 		if (player1.board.areAllSunk()) {
 			setInstruction("Player2 won!");
-			board1.removeEventListener("click", board1ClickHandler);
-		} else toggleActivePlayer();
-		renderUi(player1, player2);
+		} else showPassDevice();
+		board1.removeEventListener("click", board1ClickHandler);
+		renderUi(player1,player2)
 	}
 }
 
